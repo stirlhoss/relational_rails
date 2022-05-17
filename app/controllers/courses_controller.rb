edb55a6eec52dd5d6e1776ca7b688fdb2599c73e
Stirling Hostetter <stirhoss@icloud.com>
2022-05-17 01:20:29 -0500

    Adds form to edit course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -11,7 +11,17 @@ class CoursesController < ApplicationController
 
   def create
     course = Course.create(course_params)
-    redirect_to "/courses"
+    redirect_to '/courses'
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    course = Course.find(params[:id])
+    course.update(course_params)
+    redirect_to '/courses'
   end
 
   def course_params
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get '/courses', to: 'courses#index'
   get '/courses/new', to: 'courses#new'
   post '/courses', to: 'courses#create'
+  get '/courses/:id/edit', to: 'courses#edit'
+  patch '/courses/:id', to: 'courses#update'
   get '/courses/:id', to: 'courses#show'
 
   get '/holes', to: 'holes#index'
