f822200881dd1137d52e0cfff4ec2e2bfaf30433
Stirling Hostetter <stirhoss@icloud.com>
2022-05-17 14:45:47 -0500

    Complete user story 13

--- app/controllers/course_holes_controller.rb
+++ app/controllers/course_holes_controller.rb
@@ -2,4 +2,25 @@ class CourseHolesController < ApplicationController
   def index
     @course = Course.find(params[:parent_id])
   end
+
+  def new
+    @course = Course.find(params[:parent_id])
+  end
+
+  def create
+    hole = Hole.create(hole_params)
+    redirect_to "/courses/#{hole.course_id}/holes"
+
+  end
+
+  private
+
+  def hole_params
+    params.permit(:name,
+                  :par,
+                  :distance_to_pin,
+                  :permanent,
+                  :course_id)
+
+  end
 end
--- spec/features/course_holes/new_spec.rb
+++ spec/features/course_holes/new_spec.rb
@@ -11,5 +11,21 @@ RSpec.describe 'CourseHoles#New' do
     expect(current_path).to eq "/courses/#{course.id}/holes/new"
   end
 
+  it 'can create a new hole' do
+    course = Course.create(name: 'Megido', difficulty: 4, par: 4, permanent: true)
+    visit "/courses/#{course.id}/holes/new"
 
+    fill_in('Name', with: 'this is a test hole')
+    fill_in('Par', with: '5')
+    fill_in('Distance to pin', with: 500)
+    fill_in('Permanent', with: true)
+    click_button('Create Hole')
+
+    expect(current_path).to eq("/courses/#{course.id}/holes")
+
+    expect(page).to have_content('this is a test hole')
+    expect(page).to have_content('Par: 5')
+    expect(page).to have_content('Distance to pin: 500')
+    expect(page).to have_content('Permanent: true')
+  end
 end
